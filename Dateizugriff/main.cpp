81ef2d99ce37b0e26de697b0b92b19f5ec52a4f7
cu3l3bre <cu3l3bre@googlemail.com>
2019-05-21 08:59:31 +0200

    added some cin and couts

--- Dateizugriff/main.cpp
+++ Dateizugriff/main.cpp
@@ -9,8 +9,24 @@ int main()
 	cout << "Programm zu Dateizugriff" << endl << endl;
 
 
+	string einWort = "Hallo";
+	cout << einWort << endl;
+
+	string ganzerSatz = "Bli bla blub";
+	cout << ganzerSatz << endl;
+
+	cout << "Bitte geben Sie ein Wort ein: ";
+	cin >> einWort;
+	cout << einWort << endl;
 
 
+	cout << "Bitte geben Sie einen Satz ein: ";
+	// Hilfsangabe, wenn cin und getline zusammen benutzen,um das ENTER aus dem cin weg zu bekommen
+	cin.ignore();
+	getline(cin, ganzerSatz);
+	cout << ganzerSatz << endl;
+
+	
 	system("pause");
 	return 0;
 }
