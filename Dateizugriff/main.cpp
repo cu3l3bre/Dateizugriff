8174b450197eaf8b47617df0a8d74d35bec07466
cu3l3bre <cu3l3bre@googlemail.com>
2019-05-21 11:28:31 +0200

    added some code

--- Dateizugriff/main.cpp
+++ Dateizugriff/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <string>
+#include <fstream>	// filestream; Bibliothek zur Arbeit mit Dateien
+#include <vector>
+
+
 
 using namespace std;
 
@@ -8,6 +12,70 @@ int main()
 {
 	cout << "Programm zu Dateizugriff" << endl << endl;
 
+	string pathToOutputFile = "test.txt";
+
+	// Objekt erstellen, mit dem wir mit einer Datei arbeien können
+
+	// outputfilestream
+	ofstream ausgabeStrom;
+	ausgabeStrom.open(pathToOutputFile);
+	ausgabeStrom << "Hallo das ist ein bla " << endl;
+	ausgabeStrom << "hoi hoi hoi " << endl;
+	ausgabeStrom << "beep beep beep " << endl;
+	ausgabeStrom.close();
+
+
+
+	string pathToInputFile = "test2.txt";
+	string eineZeile = "";
+	// inputfilestream
+	ifstream eingabeStrom;		// Erzeugen eines Objektes mit Standard Kontruktor
+	//ifstream eingabeStrom("test.txt"); // Benutzen eines allg. Konstruktors, wo der Pfad direkt mit übergeben wird
+	vector<string> dateiInhalt;
+
+
+	
+	eingabeStrom.open(pathToInputFile);
+
+	// Prüfen, ob wir mit Datei arbeiten können
+	// 1. Ist die Datei vorhanden?
+	// 2. Kann die Datei geöffnet werden? Fehler? Kaputt?
+	// 3. Dateiende noch nicht erreicht (EOF)?
+
+	if (eingabeStrom.good())
+	{
+		
+		while (!eingabeStrom.eof()) // gibts auch noch, nur zur info für mich, man könnte abr auch schlichtt das .good() in die while setzen
+		{
+			//eingabeStrom >> test;				// anlalog zu cin
+			getline(eingabeStrom, eineZeile);	// Zeile einlesen
+			dateiInhalt.push_back(eineZeile);	// Zeile ans Ende der Liste anfügen
+		}
+		eingabeStrom.close();
+	}
+
+	cout << "Ausgabe der for-loop" << endl << endl;
+	for (int i = 0; i < dateiInhalt.size(); i++)
+	{
+		cout << dateiInhalt[i] << endl;
+	}
+
+
+
+	// while (getline(eingabeStrom, eineZeile));	// kurze Variante von Matus 
+	
+
+	
+	
+
+
+
+
+
+	//-----------------------------------------------------------------------------------------------------------//
+	/*
+
+	// Zum Abgleich mit der Konsole
 
 	string einWort = "Hallo";
 	cout << einWort << endl;
@@ -25,7 +93,7 @@ int main()
 	cin.ignore();
 	getline(cin, ganzerSatz);
 	cout << ganzerSatz << endl;
-
+	*/
 	
 	system("pause");
 	return 0;
